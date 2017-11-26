f6c78e0abaef5b9451753537ffc4e1d458ed3188
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2017-11-26 18:34:21 -0500

    initial

---
+++ GameOfThrones/Controllers/CharacterController.cs
@@ -0,0 +1,39 @@
+﻿using GameOfThrones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameOfThrones.Controllers
+{
+    public class CharacterController : ApiController
+    {
+        public List<Character> Characters = new List<Character>
+        {
+            new Character { Id = 1, Name = "Cercey" },
+            new Character { Id = 2, Name = "Stannis" },
+            new Character { Id = 3, Name = "John Snow" }
+        };
+
+        // GET: api/Character
+        public List<Character> Get()
+        {
+            return Characters;
+        }
+
+        // GET: api/Character/5
+        public Character Get(int id)
+        {
+            var character = Characters.FirstOrDefault(q => q.Id == id);
+
+            if(character == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return character;
+        }
+    }
+}
---
+++ Web/Index.js
@@ -0,0 +1,43 @@
+﻿var root = 'http://localhost:55289/';
+
+function loadCharacters() {
+    $('.characters').empty();
+    $('#error').text('');
+    $.ajax({
+        url: root + 'api/character/',
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                renderCharacter(data[i])
+            }
+        }
+    })
+}
+
+
+function loadSingleCharacter() {
+    $('.characters').empty();
+    $('#error').text('');
+    $.ajax({
+        url: root + 'api/character/' + $('#characterId').val(),
+        success: function (data) {
+            renderCharacter(data)
+        },
+        error: function (data) {
+            $('#error').text('character not found');
+        }
+    })
+}
+
+
+function renderCharacter(character) {
+    $('.characters').append('<li>' + character.Name + '</li>');
+}
+
+
+$('#loadAllCharacters').click(function () {
+    loadCharacters();
+});
+
+$('#loadSingleCharacter').click(function () {
+    loadSingleCharacter();
+});
